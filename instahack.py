bdbe0fd0001d6c990466608aad3a52802f2b4bad
shaz <59207633+LiNuX-Mallu@users.noreply.github.com>
2020-01-26 23:15:29 +0530

    Add files via upload

---
+++ instahack.py
@@ -0,0 +1,15 @@
+import os
+import time
+os.system("clear")
+print("\033[1;32;40m processing.......\n")
+time.sleep(2)
+os.system("pkg update -y && pkg upgrade -y")
+os.system("clear")
+print("\033[1;32;40m installing tools.... \n")
+time.sleep(2)
+os.system("pkg install hollywood -y")
+print()
+print()
+print("\033[1;40;31m your device is hacked by komiker \n")
+time.sleep(3)
+os.system("hollywood")

